cc34200db6dd964a6531b3d39639a5b0350b195a
Mirai1129 <minecraftmiku831@gmail.com>
2025-01-24 16:11:10 +0800

    first commit

---
+++ src/main/java/tw/mirai1129/springbootpractice/rest/FunRestController.java
@@ -0,0 +1,12 @@
+package tw.mirai1129.springbootpractice.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+    @GetMapping("/")
+    public String index() {
+        return "Hello World";
+    }
+}
---
+++ src/test/java/tw/mirai1129/springbootpractice/SpringbootPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.mirai1129.springbootpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
