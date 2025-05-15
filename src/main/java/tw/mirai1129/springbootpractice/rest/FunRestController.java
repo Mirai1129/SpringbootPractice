69780aad04d86a531691366a93cb40064f2d94fb
YuChia Ma <minecraftmiku831@gmail.com>
2025-05-15 21:37:33 +0800

    feature: load properties by using `@Value`

--- src/main/java/tw/mirai1129/springbootpractice/rest/FunRestController.java
+++ src/main/java/tw/mirai1129/springbootpractice/rest/FunRestController.java
@@ -1,10 +1,20 @@
 package tw.mirai1129.springbootpractice.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class FunRestController {
+    @Value("${team.name}")
+    private String teamName;
+
+    @Value("${coach.name}")
+    private String coachName;
+
     @GetMapping("/")
     public String index() {
         return "Hello World";
@@ -18,5 +28,20 @@ public class FunRestController {
     @GetMapping("/lucky")
     public String getDailyLucky() {
         return "You are very lucky!";
+    }
+
+    @GetMapping("/teaminfo")
+    public Map<String, Object> getTeamInfo() {
+        Map<String, Object> response = new HashMap<>();
+
+        Map<String, String> team = new HashMap<>();
+        team.put("name", teamName);
+        response.put("team", team);
+
+        Map<String, String> coach = new HashMap<>();
+        coach.put("name", coachName);
+        response.put("coach", coach);
+
+        return response;
     }
 }
